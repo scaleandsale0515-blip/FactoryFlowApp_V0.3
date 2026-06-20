0cabebd07c2c815c1338924bfbfefa00b6ab2d9d
scaleandsale0515-blip <scaleandsale0515@gmail.com>
2026-06-20 15:06:41 +0530

    Create edit_production_entry.dart

---
+++ lib/screens/production/edit_production_entry.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../../database/database_helper.dart';
+import '../../services/stock_service.dart';
+import '../../services/excel_service.dart';
+import '../../utils/app_theme.dart';
+import '../../utils/app_strings.dart';
+import '../../widgets/common_widgets.dart';
+
+class _EditProductionEntry extends StatefulWidget {
+  final Map<String, dynamic> prod; final List<Map<String, dynamic>> items;
+  const _EditProductionEntry({required this.prod, required this.items});
+  @override
+  State<_EditProductionEntry> createState() => _EditProductionEntryState();
+}
