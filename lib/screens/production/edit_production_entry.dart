df17a30e9f619510c9afd4c0281a07847064e16a
scaleandsale0515-blip <scaleandsale0515@gmail.com>
2026-06-21 14:42:13 +0530

    Update edit_production_entry.dart

--- lib/screens/production/edit_production_entry.dart
+++ lib/screens/production/edit_production_entry.dart
@@ -9,7 +9,7 @@ import '../../widgets/common_widgets.dart';
 
 class EditProductionEntry extends StatefulWidget {
   final Map<String, dynamic> prod; final List<Map<String, dynamic>> items;
-  const _EditProductionEntry({required this.prod, required this.items});
+  const EditProductionEntry({required this.prod, required this.items});
   @override
   State<EditProductionEntry> createState() => _EditProductionEntryState();
 }
